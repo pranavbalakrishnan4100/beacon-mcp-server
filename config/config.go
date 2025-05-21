d7c10ddd39f4a190bb9784ce076aa7464bedd818
Pranav Balakrishnan <pranav.balakrishnan@gmail.com>
2025-05-21 13:40:22 +0530

    removed slack key

--- config/config.go
+++ config/config.go
@@ -1,7 +1,1 @@
 package config
-
-// Slack config.
-const SLACK_USER_OAUTH_TOKEN = "xoxp-8811934186997-8811934228885-8826515839153-85a05274ce235be2d04fca4ba6e2f1e9"
-const SLACK_BOT_OAUTH_TOKEN = "xoxb-8811934186997-8826515934801-3mr1kudCFeUXnfuWxhlVySAp"
-
-const beacon_summariser_key = "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"
